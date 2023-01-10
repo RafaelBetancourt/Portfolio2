7b16b961affdfa29e8e60ccab9f6635415b4e084
RafaelBetancourt <rafael18282@gmail.com>
2023-01-09 22:12:24 -0500

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,156 @@
+let resumeData = {
+    "imagebaseurl":"https://www.linkedin.com/in/rafaelgbetancourto/",
+    "name": "Rafael Betancourt",
+    "role": "Frontend Developer and System Engineer",
+    "email":" rafael18282@gmail.com",
+    "instagram":" RafaGBetancourt",
+    "phone":" +51917341261",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, translate articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rafaelgbetancourto/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/RafaelBetancourt",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/rafael18282",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I'm currently looking for my first job experience . I am a full stack web developer graduated from henry's bootcamp, currently diving deeper in several technologies such as MongoBD, Typescript and NextJS, amog others. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Peru",
+    "website":"https://www.linkedin.com/in/rafaelgbetancourto/",
+    "education":[
+      {
+        "UniversityName":"Instituto Universitario Politecnico Santiago Mariño",
+        "specialization":"Systems engineering",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2017",
+        "Achievements":"Theory, Design and analysis of systems as well as Management of operating systems, creation and management of relational databases, understanding and application of complex mathematics, data structures and discrete structure and graphs."
+      },
+      {
+        "UniversityName":"Henry's Bootcamp",
+        "specialization":"Full Stack Web Developer",
+        "MonthOfPassing":"Oct",
+        "YearOfPassing":"2022",
+        "Achievements":"Full stack web developer with the PERN stack (React JS, Node JS, Express, PostgreSQL). Development of both technical and soft skills during the bootcamp that allow me today to face any space with new challenges within programming"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Freelance",
+        "specialization":"Web developer",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"2022",
+        "Achievements":"Design and developing of various single page applications"
+      },
+      {
+        "CompanyName":"Henry",
+        "specialization":"Web Developer",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2023",
+        "Achievements":"Design and development of a SPA as a requirement to opt for the full stack web developer certificate in which you can see, filter, add to favorites and create your own training routine taking into account your gender, age, equipment and experience, as well It has a payment gateway to become premium and obtain greater benefits. In this application I contributed my knowledge in React JS, CSS, Styled-Components, TailwindCSS, among others."
+      },
+      {
+        "CompanyName":"Henry",
+        "specialization":"Full-Stack Teaching Assistant (TA) for Web Development Students",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2023",
+        "Achievements":"Among the main tasks: coordinate a group of students to achieve integration into the study group, guide students in the first steps of the course, help solve exercises and promote group collaboration (Programming in pairs), propose ideas to improve Bootcamp processes."
+      },
+      {
+        "CompanyName":"Freelance",
+        "specialization":"Business Inteligence Assistant",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2019",
+        "Achievements":"ETL procedures in Oracle PL/SQL, design and development of dashboards in Microstrategy."
+      },
+      {
+        "CompanyName":"Proamsa",
+        "specialization":"IT Assistant (Internships)",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2015",
+        "Achievements":"Assistant of the information technology department, in charge of installation, maintenance and repair of the company's technological equipment."
+      }
+    ],
+    //"skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"TailwindCSS"
+      },
+      {
+        "skillname":"Redux"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Expressjs"
+      },
+      {
+        "skillname":"sql"
+      },
+      {
+        "skillname":"postgresql"
+      },
+      {
+        "skillname":"mongodb"
+      },
+      {
+        "skillname":"typescript"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"CountryApp",
+        "description":"Single Page Application",
+        "imgurl":"images/portfolio/countries.jpg"
+      },
+      {
+        "name":"Fit Focus",
+        "description":"Single Page Application",
+        "imgurl":"images/portfolio/nav-icon.png"
+      },
+      {
+        "name":"Weather App",
+        "description":"Single Page Application",  
+        "imgurl":"images/portfolio/weather.jpg"
+      },
+      {
+        "name":"Todo App",
+        "description":"Single Page Application",
+        "imgurl":"images/portfolio/todo.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a great guy",
+        "name":"David I. Cadenas"
+      },
+      {
+        "description":"This is a great partner",
+        "name":"Paola F. Vargas"
+      }
+    ]
+  }
+  
+  export default resumeData
