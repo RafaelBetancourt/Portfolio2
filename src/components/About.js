636ae59cf15859dbb2c935f90ed122ac24f0ef6b
RafaelBetancourt <rafael18282@gmail.com>
2023-01-13 18:16:14 -0500

    spaces erased

--- src/components/About.js
+++ src/components/About.js
@@ -13,18 +13,14 @@ export default class About extends Component {
             </div>
 
             <div className="nine columns main-col">
-
                <h2>About Me</h2>
                <p>
                {
                  resumeData.aboutme
                }
                </p>
-
                <div className="row">
-
                   <div className="columns contact-details">
-
                   <h2>Contact Details</h2>
                   <p className="address">
        						<span>{resumeData.name}</span>
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,7 +10,6 @@ export default class Resume extends Component {
               <span>Education</span>
             </h1>
           </div>
-
           <div className="nine columns main-col">
             {resumeData.education &&
               resumeData.education.map((item) => {
@@ -38,7 +37,6 @@ export default class Resume extends Component {
               <span>Work</span>
             </h1>
           </div>
-
           <div className="nine columns main-col">
             {resumeData.work &&
               resumeData.work.map((item) => {
@@ -60,17 +58,14 @@ export default class Resume extends Component {
               })}
           </div>
         </div>
-
         <div className="row skill">
           <div className="three columns header-col">
             <h1>
               <span>Skills</span>
             </h1>
           </div>
-
           <div className="nine columns main-col">
             <p>{resumeData.skillsDescription}</p>
-
             <div className="bars">
               <ul className="skills">
                 {resumeData.skills &&
